e77a84cadf3585ae9b564900206a735748b63c20
Mr J Bennett <jbennett@birkdalehigh.co.uk>
2015-05-06 10:22:59 +0100

    Code comment to describe the file and its intent.

--- NewPolicyMap.ps1
+++ NewPolicyMap.ps1
@@ -1,3 +1,9 @@
+<#
+Create a table to lookup what GPO name means what ID folder in the sysvol path for debugging.
+
+This file is left in our administrator documents area for group policy notes, 
+it is intended to just be "right-click" and ran with powershell to easily update the list by anyone.
+#>
 $domain = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().name
 
 get-childitem -path "\\$domain\sysvol\$domain\Policies" -Exclude PolicyDefinitions -Directory | 
