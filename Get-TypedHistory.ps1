3b48e4e8e6b3d120084eb7147859d41957092f31
Mr J Bennett <jbennett@birkdalehigh.co.uk>
2015-09-24 15:26:01 +0100

    more refined test query.

--- Get-TypedHistory.ps1
+++ Get-TypedHistory.ps1
@@ -1,1 +1,4 @@
-﻿Get-WinEvent -LogName "Windows PowerShell" -ComputerName u05pc02 | select -first 9 | % { ([xml]$_.ToXml()).Event.EventData.Data[0] }
+﻿Get-WinEvent -FilterHashtable @{
+    LogName = "Windows PowerShell";
+    ID=800;
+} -ComputerName "u05pc19"| select -first 50 | % { ([xml]$_.ToXml()).Event.EventData.Data[2] }
