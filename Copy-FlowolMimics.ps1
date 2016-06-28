4d544e1c7bf40c890562ab3abc69ec5634d12f75
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-06-28 13:20:07 +0100

    Put Program files into installed application

---
+++ Copy-FlowolMimics.ps1
@@ -0,0 +1,8 @@
+﻿param(
+    [paramter(mandatory=$true)]
+    $Path
+        
+    , $Destination = "${env:ProgramFiles(x86)}\Keep I.T. Easy\Flowol 3\Mimics"
+)
+
+copy-item -Recurse -Force -Path $path -Destination $Destination
