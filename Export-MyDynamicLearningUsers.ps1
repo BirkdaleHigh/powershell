aef5251903737017d45a065ef2e04204e690da6a
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-06-28 12:08:49 +0100

    Re-write to put each converted user into the pipeline.

--- Export-MyDynamicLearningUsers.ps1
+++ Export-MyDynamicLearningUsers.ps1
@@ -1,4 +1,4 @@
-Param(
+<#Param(
     $SearchBase = "OU=Year R,OU=Students,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL",
     $MDLGroup = "Year 10 | Mr J Bennett",
     $Path = 'export.csv'
@@ -22,3 +22,55 @@ get-aduser -Filter * -SearchBase $SearchBase -Properties 'givenname','surname',S
         @{n=$MDLGroup;e={'Yes'}} |
         ConvertTo-Csv -NoTypeInformation |
         out-file $Path
+        #>
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Validate-User
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Action
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [ValidateSet("A", "E", "D")]
+        [Alias('Action')]
+        [string]
+        ${1 - Action} = "A"
+
+        , # User ID - do not edit (DL use only)
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string]
+        ${2 - User ID - do not edit (DL use only)}
+
+        , # S (for ‘student’) or TA (for Teacher)
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [ValidateSet("S", "TA")]
+        [string]
+        ${3 - User Type} = "S"
+
+        , # User Name
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [ValidateLength(6,25)]
+        [Alias('SamAccountName')]
+        [string]
+        ${4 - User Name}
+
+        , # User ID - do not edit (DL use only)
+        [Parameter(ValueFromPipelineByPropertyName=$true)]
+        [string]
+        ${5 - Passwword} = (Get-Random -Minimum 1000 -Maximum 9999)
+    )
+    Process
+    {
+        New-Object -TypeName pscustomobject -Property $PSBoundParameters
+    }
+}
