a77d3649d001162ebb3f569a56418d13cf718598
Jackbennett <jbennett@birkdalehigh.co.uk>
2017-01-20 14:05:28 +0000

    fix broken quotes

--- Fix-AMDJpegDecoder.ps1
+++ Fix-AMDJpegDecoder.ps1
@@ -9,8 +9,8 @@
         $Session
     )
     Begin {
-        if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]“Administrator”)){
-            Write-Warning “You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!”
+        if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){
+            Write-Warning "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
             Break
         }
     }
