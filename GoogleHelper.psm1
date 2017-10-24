c6a95290ca5f4fccf8e6f7653e7e67a1834a4f36
Jackbennett <jbennett@birkdalehigh.co.uk>
2017-10-24 10:58:08 +0100

    New Task to register for clearing the cache's used by sync-cmd
    Teach the powershell wrapper to use --flush and add a task to call it.

--- GoogleHelper.psm1
+++ GoogleHelper.psm1
@@ -7,20 +7,28 @@ function Start-DirectorySync {
         # Ignore sync impact limits
         [switch]
         $force
+
+        , # Clear Cache
+        [switch]
+        $Flush
     )
 
-    callSyncTool -Start -Force:$force
+    callSyncTool -Start -Force:$Force -Flush:$Flush
 }
 
 function callSyncTool {
     Param(
         [switch]
         $Start
+
         ,[switch]
         $Force
 
         ,[switch]
         $Test
+
+        ,[switch]
+        $Flush
 
         ,[string]
         $WorkingDirectory = "C:\GoogleData"
@@ -42,6 +50,7 @@ function callSyncTool {
     }
     if($Start){ $process.ArgumentList += "--apply" }
     if($Force){ $process.ArgumentList += "--deletelimits" }
+    if($Flush){ $process.ArgumentList += "--flush" }
 
     start-process @process
 }
@@ -53,6 +62,7 @@ function Register-DirectorySync {
     .DESCRIPTION
         Create a "test" job that will not make any changes to google. This job must be run on demand.
         Create a "start" job that will apply changes to google and be scheduled for 6am everyday. This job can be run on demand.
+        Create a "refresh" job the will flush the cashes' used by sync-cmd when running. Will take longer to sync.
 
         Call Start-DirectorySync directly from this module to get access to the "-Force" option which will ignore the delete limits in google.
         This will let you wipe the whole G-Suite service data so make sure you know what you're doing.
@@ -73,11 +83,12 @@ function Register-DirectorySync {
     .OUTPUTS
         Registered task in the folder \Google
     .NOTES
-        General notes
+        sync-cmd documentation: https://support.google.com/a/answer/6152425?hl=en
+        Directory Sync release notes: https://support.google.com/a/answer/1263028?hl=en
     #>
     param(
         [parameter(Mandatory)]
-        [ValidateSet("Test", "Schedule")]
+        [ValidateSet("Test", "Schedule","Refresh")]
         [string]
         $Type
     )
@@ -88,11 +99,15 @@ function Register-DirectorySync {
         "Test" {
             $action.Arguments = "-command `"& {import-module .\GoogleHelper\GoogleHelper.psm1; Test-DirectorySync}`""
             Register-ScheduledTask -TaskName "Test G-Suite Sync" -TaskPath "Google" -Action $action -Description "Test syncing action between active directory and google"
-         }
+        }
         "Schedule" {
             $action.Arguments = "-command `"& {import-module .\GoogleHelper\GoogleHelper.psm1; Start-DirectorySync}`""
             Register-ScheduledTask -TaskName "Start G-Suite Sync" -TaskPath "Google" -Action $action -Description "Start syncing action between active directory and google" -Trigger $trigger
-         }
+        }
+        "Refresh" {
+            $action.Arguments = "-command `"& {import-module .\GoogleHelper\GoogleHelper.psm1; Start-DirectorySync -Flush}`""
+            Register-ScheduledTask -TaskName "Flush G-Suite Sync Cache" -TaskPath "Google" -Action $action -Description "Sync and flush teh cache between active directory and google"
+        }
         Default { Write-Error "Select either 'Test' or 'Schedule' job types to register"}
     }
 
