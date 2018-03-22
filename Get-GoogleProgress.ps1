b713c8c888a5be89ca355549aa2e411aec3268b9
Jackbennett <jbennett@birkdalehigh.co.uk>
2018-03-22 16:07:39 +0000

    Add student years to breakdown and order object output.

--- Get-GoogleProgress.ps1
+++ Get-GoogleProgress.ps1
@@ -21,15 +21,26 @@ function Get-GoogleProgress {
     $Students = Get-aduser -filter $Filter -SearchBase 'OU=Students,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
     $Staff = Get-aduser -filter $Filter -SearchBase 'OU=Staff,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
     $Office = Get-aduser -filter $Filter -SearchBase 'OU=Office,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
+    $2013 = Get-aduser -filter $Filter -SearchBase 'OU=2013,OU=Students,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
+    $2014 = Get-aduser -filter $Filter -SearchBase 'OU=2014,OU=Students,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
+    $2015 = Get-aduser -filter $Filter -SearchBase 'OU=2015,OU=Students,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
+    $2016 = Get-aduser -filter $Filter -SearchBase 'OU=2016,OU=Students,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
+    $2017 = Get-aduser -filter $Filter -SearchBase 'OU=2017,OU=Students,OU=Users,OU=BHS,DC=BHS,DC=INTERNAL' -properties PasswordLastSet
 
     function percent([int]$a, [int]$b){
         [Math]::Round( ($b / $a) * 100, 2)
     }
 
-    New-Object PSObject -Property @{
+    $table = [ordered]@{
         "Date"     = Get-Date
         "Students" = percent ($Students | Measure-Object).count ($Students | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
         "Staff"    = percent ($Staff    | Measure-Object).count ($Staff    | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
         "Office"   = percent ($Office   | Measure-Object).count ($Office   | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
+        "2013"     = percent ($2013     | Measure-Object).count ($2013     | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
+        "2014"     = percent ($2014     | Measure-Object).count ($2014     | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
+        "2015"     = percent ($2015     | Measure-Object).count ($2015     | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
+        "2016"     = percent ($2016     | Measure-Object).count ($2016     | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
+        "2017"     = percent ($2017     | Measure-Object).count ($2017     | Where-Object PasswordLastSet -gt $GoogleDate | Measure-Object).count
     }
+    New-Object PSCustomObject -Property $table
 }
