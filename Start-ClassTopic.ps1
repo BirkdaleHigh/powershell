2e628317df27a3e828fefcd2ef314e4d4c8fa507
Jackbennett <jbennett@birkdalehigh.co.uk>
2018-06-18 10:50:18 +0100

    New help for Start-ClassTopic and -Open can use full or relative paths

--- Start-ClassTopic.ps1
+++ Start-ClassTopic.ps1
@@ -1,16 +1,38 @@
+<#
+.SYNOPSIS
+    Copy paths to a destination when run
+.DESCRIPTION
+    Script cmdlet to be used as a shortcut target for automatically copying work for users as starters t o activities.
+
+    Powershell swtiches of interest are;
+    * -NoLogo to elimintate the powershell copywrite.
+    * -NoExit to leave the console open, usefull for command line lessons.
+    * -NonInteractive, combined with noExit can leave the actions taken open for users to see.
+
+    Anything after -File is interpreted for this script and not powershell.exe therefore it must be at the end of the above options.
+
+    Be careful of the path field length limitations when creating shortcut files. You might have to create intermediate script files to call this file instead.
+.EXAMPLE
+    PS C:\> Powershell.exe -NoExit -NoLogo -File "\\applications\path\Start-ClassTopic.ps1" -Path "\\files\share\lesson\*" -Destination "~\lesson1" -Open "readme.docx" -Show
+    Execute this file in powershell, without exiting the console to show the user where files have been copied to in their own area. If called again will open "readme.docx" instead.
+
+    Create a shortcut with this as the target at a location users look to start programs,
+    they can quickly jumpstart their lesson by getting resources they will need automatically.
+#>
 Param(
+    # Files or filter to source files from.
     [string[]]
-    $Path = @("W:\Computing\Topic 12\CL\*", "W:\Computing\Topic 12\Task 12Aiv.docx" )
+    $Path
 
-    , # Folde to put the work into
+    , # Folder to copy files into
     [string]
-    $Destination = "N:\Topic"
+    $Destination
 
-    , #Open the topic start file if the templates already exist.
+    , # File or path to open when already copied. Full Path or in destination.
     [string]
-    $Open = "W:\Computing\Topic 12\Task 12Aiv.docx"
+    $Open
 
-    , # Show the copied items.
+    , # Show the copied items as output.
     [Alias('Show')]
     [switch]
     $PassThru
@@ -21,7 +43,9 @@ try {
 } catch {
     Write-Host "You already have a Folder at: $Destination, This will not copy the files again, carry on." -ForegroundColor Green
     if($Open){
-        Start-process (Join-Path $Destination $Open)
+        Push-Location $Destination
+        Start-Process $Open
+        Pop-Location
     }
     return # Exit here if the folder already exists
 }
